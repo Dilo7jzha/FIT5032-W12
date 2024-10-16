8da2062a3ad1f2e87d5c0e20b62e5889fad2b0ed
Dilo7jzha <whateverittakes23@outlook.com>
2024-10-16 19:30:17 +1100

    Trigger deployment

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '',
+  build: {
+    outDir: 'dist'
+  },
   plugins: [
     vue(),
     vueDevTools(),
